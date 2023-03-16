b27fcb741aaf7803883ecc4e935345e9675e28d7
Ibyle <veektorscreation@gmail.com>
2023-03-16 07:48:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - This program prints a line using the function put 
- * Return: 0(success)
- */	
-int main(void) 
+ * main-This program prints a line using the function put
+* Return:0(success)
+*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
