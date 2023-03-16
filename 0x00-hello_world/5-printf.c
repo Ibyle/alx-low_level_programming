9442efb2800af29b099510b8dd421d77a0eeabcf
Ibyle <veektorscreation@gmail.com>
2023-03-16 08:45:12 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -This program prints exactly a line using printf
+ * Return: 0(success)
+ */
+int main(void)
+{
+printf("\"with proper grammar, but the outcome s a piece of art,\n");
+return (0);
+}
