0863e1e6a91296664d2615c58d576c67ff4d43ea
Ibyle <veektorscreation@gmail.com>
2023-03-16 15:56:49 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - This is a C program that prints a line to the standard erro
- * Return 1 (Success)
- */
+ * main - This is a C program that prints a line to the standard error
+ * Description: 'the program's description'
+ *
+ * Return: 1 (Success)
+*/
+
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
