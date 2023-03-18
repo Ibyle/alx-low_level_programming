25082ca48926c3b8c78c0b28b9a048ad58efbc36
Ibyle <veektorscreation@gmail.com>
2023-03-18 00:42:50 -0700

    0x01. C - Variables, if, else, while C

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - prints all possible combinations of two two-digit numbers
+* main - iiprints all possible combinations of two two-digit numbers
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-int i, j;
+	int i, j;
 
 for (i = 0; i < 100; i++)
 {
@@ -18,7 +18,7 @@ putchar((i / 10) + 48);
 putchar((i / 10) + 48);
 putchar(' ');
 putchar((j / 10) + 48);
-putchar((i % 10) + 48);
+putchar((j % 10) + 48);
 if (i != 98 || j != 99)
 {
 putchar(',');
@@ -27,7 +27,7 @@ putchar(' ');
 }
 }
 }
-putchar('\');
+putchar('\n');
 return (0);
 }
 
