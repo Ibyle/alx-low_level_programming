445b2b5358b965afc657afe27b4bb874551f05f7
Ibyle <veektorscreation@gmail.com>
2023-03-21 15:00:25 -0700

    loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,6 @@
 #include "main.h"
 /**
  * main - print _putchar
-	 *
-	 * Description: 'this program describe'
-	 * @parameter: descrive the parameter
 	 * is written by Victor Michael
 	 * Return: 0
 */
